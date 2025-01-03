aa4a64d5b806422a29254ca546d180ee7efe20dc
bchan <cmhbenjamin@gmail.com>
2025-01-02 18:00:20 -0800

    Moving page content editing to frontmatter cms

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,8 @@
-export default new Map();
+
+export default new Map([
+["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
+["src/content/pages/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/pages/colophon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fcolophon.mdx&astroContentModuleFlag=true")],
+["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")],
+["src/content/pages/resume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fresume.mdx&astroContentModuleFlag=true")]]);
+		
---
+++ src/content.config.ts
@@ -0,0 +1,28 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      lastmod: z.date(),
+      description: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      tags: z.array(z.string())
+    })
+});
+const pages = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/pages" }),
+    schema: z.object({
+      title: z.string(),
+      lastmod: z.date(),
+    })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog, pages };
