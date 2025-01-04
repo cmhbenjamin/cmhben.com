43fed6decffd3cfa740bc330e68ad9525e09fae4
bchan <cmhbenjamin@gmail.com>
2025-01-03 22:37:24 -0800

    adding content in colophon

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -3,6 +3,7 @@ export default new Map([
 ["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/colophon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fcolophon.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/pages/resume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fresume.mdx&astroContentModuleFlag=true")],
-["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/reel_&_resume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Freel_%26_resume.mdx&astroContentModuleFlag=true")],
+["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")],
+["src/content/pages/resume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fresume.mdx&astroContentModuleFlag=true")]]);
 		
