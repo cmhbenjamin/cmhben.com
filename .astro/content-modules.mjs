baecf557ba1949f8498b8a01335a107da37a3fd1
bchan <cmhbenjamin@gmail.com>
2025-01-16 16:51:00 -0800

    updated some content and styling

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,12 +1,14 @@
 
 export default new Map([
-["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
-["src/content/pages/colophon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fcolophon.mdx&astroContentModuleFlag=true")],
-["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")],
-["src/content/pages/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/pages/resume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fresume.mdx&astroContentModuleFlag=true")],
+["src/content/blog/exploring-houdini-volumes-tips-and-techniques.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fexploring-houdini-volumes-tips-and-techniques.mdx&astroContentModuleFlag=true")],
+["src/content/blog/ffmpeg-cheatsheet.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fffmpeg-cheatsheet.mdx&astroContentModuleFlag=true")],
+["src/content/blog/file-processing-with-python.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ffile-processing-with-python.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/moviepy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoviepy.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/simple-compositing-with-houdini-copernicus.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsimple-compositing-with-houdini-copernicus.mdx&astroContentModuleFlag=true")],
-["src/content/pages/pages.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fpages.mdx&astroContentModuleFlag=true")],
-["src/content/pages/posts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fposts.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
+["src/content/pages/colophon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fcolophon.mdx&astroContentModuleFlag=true")],
+["src/content/pages/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")],
+["src/content/pages/posts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fposts.mdx&astroContentModuleFlag=true")],
+["src/content/pages/resume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fresume.mdx&astroContentModuleFlag=true")]]);
 		
