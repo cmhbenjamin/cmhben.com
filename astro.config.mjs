019e0e3e1b7761c63269bead9ef3f6ea5ef3cc89
Benjamin Chan <cmhbenjamin@gmail.com>
2025-10-27 18:55:43 -0700

    Remove Sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,12 @@ import mdx from '@astrojs/mdx';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import sanity from '@sanity/astro';
+// import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(),sanity({
-      projectId: '8444qxsb',
-      dataset: "production",
-      useCdn: false, // See note on using the CDN
-      apiVersion: "2025-09-19", 
-      studioBasePath: '/studio'// insert the current date to access the latest version of the API
-    }),
-, react()],
+  integrations: [mdx()],
 
   vite: {
     plugins: [tailwindcss()]
